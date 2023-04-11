3fe69c21a509dbcd06ae994f19b333cd2368807e
salms358 <sa083850@gmail.com>
2023-04-11 15:41:57 +0000

    attempting to get both boards to print on console

--- run.py
+++ run.py
@@ -7,10 +7,16 @@ SHIP_SIZES = 3
 easy_mode = 1
 medium_mode = 2
 hard_mode = 3
-board_sizes = {easy_mode: 7, medium_mode: 9, hard_mode: 12}
+board_sizes = {"1": 7, "2": 9, "3": 12}
 comp_board = [['O' for x in range(10)] for y in range(10)]
+player_board = [['O' for x in range(10)] for y in range(10)]
 boatsz = {"Destroyer": 2, "Cruiser": 3, "Battleship": 4, "Aircraft Carrier": 5}
-print(r"""
+difficulties = ""
+
+
+def welcome():
+    """Ascii art of ship"""
+    print(r"""
    __    __    __
                              |==|  |==|  |==|
                            __|__|__|__|__|__|_
@@ -19,11 +25,10 @@ print(r"""
                     |............................o.../
                     \.............................../
                hjw_,~')_,~')_,~')_,~')_,~')_,~')_,~')/,~')_
-""")
-print("Welcome! :)")
+               """)
+    print("Welcome! :)")
 
-# game Not running after printing the ship
-#Errors with board_size function stating that it is not defined
+
 def rules():
     """ method to show the rules """
     rulez = input("Do you want to see the rules? (y/n)")
@@ -47,72 +52,73 @@ def rules():
     print("lets gooooooooooo")
 
 # Set up game board
-    def set_diff():
-        """setting difficulty"""
-        print("Select your difficulty level")
-        print("1. easy")
-        print("2. medium")
-        print("3. hard")
+
+
+def set_diff():
+    """setting difficulty"""
+    print("Select your difficulty level")
+    print("1. easy")
+    print("2. medium")
+    print("3. hard")
+    global difficulties
+    difficulties = input("Enter desired difficulty")
+    while difficulties not in ["1" , "2" , "3"]:
+        print("Invalid input")
         difficulties = input("Enter desired difficulty")
-        while difficulties not in ["1","2","3"]:
-            print("Invalid input")
-            difficulties = input("Enter desired difficulty")
-            
-            set_diff()
-            player_board = dual_boards()
-            comp_board = dual_boards()
-            print_boards(player_board, comp_board)
+        
+        #set_diff()
+        #player_board = dual_boards()
+        #comp_board = dual_boards()
+        #print_boards(player_board, comp_board)
 
-    def dual_boards():
-        board = []
-        for i in range(board_size):
-            row = ["O"] * board_size
-            board.append(row)
-            
-        print(f"Board size: {board_size}x{board_size}")
 
-        comp_board = []
-        for i in range(board_size):
-            row = ["O"] * board_size
-            comp_board.append(row)
-            return comp_board
+def dual_boards():
+    """
+    This should lead to both board printing at the same time
+    """
+    board = []
+    for i in range(board_sizes[difficulties]):
+        row = ["O"] * board_sizes[difficulties]
+        board.append(row)
 
-    def print_boards(player_board, comp_board):
-        """
-        Print both sets of boards
-        """
-        print("Player board:")
-        for row in player_board:
-            print(" ".join(row))
-            print("Computer board:")
-            for row in comp_board:
-                #
-                print(" ".join(["O" if cell == "S" and hit else cell for cell, hit in row]))
+def print_board(board):
+    print(player_board)
+    print(comp_board)
+        
+    print(f"Board size: {board_sizes[difficulties]}x{board_sizes[difficulties]}")
+
+    comp_board = []
+    for i in range(board_sizes[difficulties]):
+        row = ["O"] * board_sizes[difficulties]
+        comp_board.append(row)
+        
     
-    dual_boards()
 
-
+    #return player_board, [], print_boards
 
 # Place ships randomly
+
+
+def ships():
     for i in range(NUMBER_SHIPS):
-        ship_row = random.randint(0, board_size - 1)
-        ship_col = random.randint(0, board_size - 1)
+        ship_row = random.randint(0, board_sizes[difficulties] - 1)
+        ship_col = random.randint(0, board_sizes[difficulties] - 1)
 
     # Ensure the ship does not overlap with another ship
-        while board[ship_row][ship_col] == "S":
-            ship_row = random.randint(0, board_size - 1)
-            ship_col = random.randint(0, board_size - 1)
+        while player_board[ship_row][ship_col] == "S":
+            ship_row = random.randint(0, board_sizes[difficulties] - 1)
+            ship_col = random.randint(0, board_sizes[difficulties] - 1)
 
         # Place the ship
         for j in range(SHIP_SIZES):
-            if ship_col + j < board_size:
-                board[ship_row][ship_col + j] = "S"
+            if ship_col + j < board_sizes[difficulties]:
+                player_board[ship_row][ship_col + j] = "S"
             else:
-                board[ship_row] + 1
+                comp_board[ship_row] = + 1
     comp_ship_placement = []
     for i in range(SHIP_SIZES):
         row = random.randint(0, SHIP_SIZES - 1)
-        col = random.randint(0, SHIP_SIZES- 1)
+        col = random.randint(0, SHIP_SIZES - 1)
         comp_ship_placement.append((row, col))
 
 
@@ -124,9 +130,11 @@ def game():
     num_hits = 0
     comp_hits = 0
     comp_misses = 0
-    while num_hits < SHIP_SIZES * NUMBER_SHIPS and num_guesses and comp_hits < board_size ** 2:
+    comp_score = 0
+    user_score = 0
+    while num_hits < SHIP_SIZES * NUMBER_SHIPS and num_guesses and comp_hits < board_sizes[difficulties] ** 2:
         # Print the game board
-        for row in board:
+        for row in player_board:
             print(" ".join(row))
 
         # Get user input
@@ -134,20 +142,20 @@ def game():
         guess_col = int(input("Guess col: "))
 
         # Check if the guess is a hit or a miss
-        if board[guess_row][guess_col] == "S":
+        if board_sizes[guess_row][guess_col] == "S":
             print("Hit!")
-            board[guess_row][guess_col] = "X"
+            board_sizes[guess_row][guess_col] = "X"
             num_hits += 1
         else:
             print("Miss!")
-            board[guess_row][guess_col] = "X"
+            board_sizes[guess_row][guess_col] = "X"
 
         num_guesses += 1
 
         #print("Computer board: ")
         #computer_board()
-        row_comp = random.randint(0, board_size-1)
-        col_comp = random.randint(0, board_size-1)
+        row_comp = random.randint(0, board_sizes[difficulties]-1)
+        col_comp = random.randint(0, board_sizes[difficulties]-1)
         def print_scores():
             if (row_comp, col_comp) in comp_ship_placement:
                 print("The computer hit your ship!")
@@ -162,16 +170,20 @@ def game():
 
     # End the game
     print("Game over!")
-    for row in board_:
+    for row in player_board:
         print(" ".join(row))
 
     print("Number of guesses:", num_guesses)
     print("Number of hits:", num_hits)
 
 
-
-
-#if __name__ == "__main__":
-    #while True:
-        
+if __name__ == "__main__":
+    while True:
+        welcome()
+        rules()
+        set_diff()
+        #ships()
+        game()
+        break 
+    
     #this is my game loop, to exit it use the break statement Thanks for playing!
