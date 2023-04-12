2bde9870ce248e5b58d7a9a7e313fa85aaa7f0ad
salms358 <>
2023-04-12 20:07:11 +0000

    recorrecting my code

--- run.py
+++ run.py
@@ -1,18 +1,7 @@
 import random
 
-# Set up game constants
-
-NUMBER_SHIPS = 5
-SHIP_SIZES = 3
-board_sizes = {"1": 7, "2": 9, "3": 12}
-comp_board = [['O' for x in range(10)] for y in range(10)]
-player_board = [['O' for x in range(10)] for y in range(10)]
-difficulties = ""
-board_size = ""
 
 # Should print out ASCII art as a welcome page
-
-
 def welcome():
     """Ascii art of ship"""
     print(r"""
@@ -25,42 +14,36 @@ def welcome():
                     \.............................../
                hjw_,~')_,~')_,~')_,~')_,~')_,~')_,~')/,~')_
                """)
-    print("Welcome! :)")
-
-# If the user wants to see the rules incase its their first time or theyneed a reminder they can ask for the rules to be printed onto the console
+    print("Welcome to the battleships game! :)")
 
 
+
+# If the user wants to see the rules incase its their first time or theyneed 
+# a reminder they can ask for the rules to be printed onto the console
 def rules():
     """ method to show the rules """
-    rulez = input("Do you want to see the rules? (y/n)")
+    rulez = input("Do you want to see the rules? (y/n) ")
     while rulez.lower() not in ["y", "n"]:
         print("invalid input")
-        rulez = input("Do you want to see the rules? (y/n)")
+        rulez = input("Do you want to see the rules? (y/n) ")
     if rulez == "y":
+        print()
         print("The goal of the game is to sink all of your opponent's ships")
-
         print("before they sink yours.")
-
         print("The ships are of different sizes and are placed")
-
         print("horizontally or vertically on the board.")
-
         print("Players take turns to guess the location of their opponent's")
-
         print("ships by calling out a coordinate (e.g.D7)")
-
         print("If the guess is a hit, the player marks that location on their")
         print("opponent's board with a marker to indicate a hit.")
-
         print("If the guess is a miss, the player marks that location with a ")
         print("marker to indicate a miss.")
-
         print("The game continues until all ships of one player have been sunk ")
         print("and the other player is declared the winner.")
+        print()
 
-    print("lets gooooooooooo")
-
-# Set up game board
+    print("lets gooooooooooo!")
+    print()
 
 
 def set_diff():
@@ -69,12 +52,11 @@ def set_diff():
     print("1. easy")
     print("2. medium")
     print("3. hard")
-    global difficulties
-    board_size = board_sizes.get(difficulties)
-    difficulties = input("Enter desired difficulty")
+    difficulties = input("Enter desired difficulty: ")
     while difficulties not in ["1", "2", "3"]:
         print("Invalid input")
-        difficulties = input("Enter desired difficulty")
+        difficulties = input("Enter desired difficulty: ")
+    return difficulties
 
 
 def dual_boards():
@@ -94,68 +76,95 @@ def dual_boards():
         comp_board.append(row)
 
 
-def print_board(board):
+def print_board(player_board, comp_board):
     """
     printing both boards together
     """
-    print(player_board)
-    print(comp_board)
+    print("Player board:")
+    for row in player_board:
+        print(" ".join(row))
+    print()
 
-    # return player_board, [], print_boards
+    print("Computer board:")
+    for row in comp_board:
+        print(" ".join(row))
+    print()
 
-# Place ships randomly
 
-
-def ships():
+def ships(board_size: int, player_board, comp_board):
+    """
+    Place ships randomly
+    """
     for i in range(NUMBER_SHIPS):
         ship_row = random.randint(0, board_size - 1)
         ship_col = random.randint(0, board_size - 1)
 
-    # Ensure the ship does not overlap with another ship
+        # Ensure the ship does not overlap with another ship
         while player_board[ship_row][ship_col] == "S":
             ship_row = random.randint(0, board_size - 1)
             ship_col = random.randint(0, board_size - 1)
 
-        # Place the ship
-        for j in range(SHIP_SIZES):
-            if ship_col + j < board_size:
-                player_board[ship_row][ship_col + j] = "S"
-            else:
-                comp_board[ship_row] = + 1
-    comp_ship_placement = []
-    for i in range(SHIP_SIZES):
-        row = random.randint(0, SHIP_SIZES - 1)
-        col = random.randint(0, SHIP_SIZES - 1)
-        comp_ship_placement.append((row, col))
+        player_board[ship_row][ship_col] = "S"
+
+    for i in range(NUMBER_SHIPS):
+        ship_row = random.randint(0, board_size - 1)
+        ship_col = random.randint(0, board_size - 1)
+
+        # Ensure the ship does not overlap with another ship
+        while player_board[ship_row][ship_col] == "S":
+            ship_row = random.randint(0, board_size - 1)
+            ship_col = random.randint(0, board_size - 1)
+
+        comp_board[ship_row][ship_col] = "S"
+
+    return player_board, comp_board
 
 
-def game():
-    """gameplay"""
-
+def game(board_size: int, comp_board):
+    """
+    Gameplay loop method
+    """
     # Play the game
-    num_guesses = 0
+    num_guesses = 8
     num_hits = 0
     comp_hits = 0
     comp_misses = 0
-    while num_hits < SHIP_SIZES * NUMBER_SHIPS and num_guesses and comp_hits < board_size ** 2:
-        # Print the game board
-        for row in player_board:
-            print(" ".join(row))
+    # while num_hits < SHIP_SIZES * NUMBER_SHIPS and num_guesses and comp_hits < board_size ** 2:
+    while num_guesses > 0:
+        # Print game boards
+        print_board(player_board, comp_board)
 
         # Get user input
-        guess_row = int(input("Guess row: "))
-        guess_col = int(input("Guess col: "))
+        try:
+            guess_row = int(input("Guess row: "))
+            guess_col = int(input("Guess col: "))
+            if guess_row <= board_size and guess_row > 0:
+                # Do nothing
+                pass
+            else:
+                raise "Number is out of bounds"
+            if guess_col <= board_size and guess_col > 0:
+                # Do nothing
+                pass
+            else:
+                raise "Number is out of bounds"
+        except ValueError:
+            print("Invalid input")
+            continue
+
+        guess_row = guess_row - 1
+        guess_col = guess_col - 1
 
         # Check if the guess is a hit or a miss
-        if board_sizes[guess_row][guess_col] == "S":
+        if comp_board[guess_row][guess_col] == "S":
             print("Hit!")
-            board_sizes[guess_row][guess_col] = "X"
+            comp_board[guess_row][guess_col] = "X"
             num_hits += 1
         else:
             print("Miss!")
-            board_sizes[guess_row][guess_col] = "X"
+            comp_board[guess_row][guess_col] = "M"
 
-        num_guesses += 1
+        num_guesses -= 1
 
         # print("Computer board: ")
         # computer_board()
@@ -164,6 +173,9 @@ def game():
 
 
 def print_scores():
+    """
+    Method to print user score
+    """
     if (row_comp, col_comp) in comp_ship_placement:
         print("The computer hit your ship!")
         board_size[row_comp][col_comp] = "X"
@@ -174,7 +186,7 @@ def print_scores():
         comp_misses += 1
 
     # End the game
-    # print("Game over!")
+    print("Game over!")
     for row in player_board:
         print(" ".join(row))
 
@@ -183,12 +195,20 @@ def print_scores():
 
 
 if __name__ == "__main__":
+    # Set up game constants
+    NUMBER_SHIPS = 5
+    SHIP_SIZES = 3
+    board_sizes = {"1": 7, "2": 9, "3": 12}
+    board_size = 0
     while True:
         welcome()
         rules()
-        set_diff()
-        ships()
-        game()
-        break
+        difficulties = set_diff()
 
-    # this is my game loop, to exit it use the break statement Thanks for playing!
+        board_size = board_sizes[difficulties]
+        comp_board = [['O' for x in range(board_size)] for y in range(board_size)]
+        player_board = [['O' for x in range(board_size)] for y in range(board_size)]
+        player_board, comp_board = ships(board_size, player_board, comp_board)
+
+        game(board_size, comp_board)
+        break
